ab0a90aa894436b259edb50ed94a33185404b732
dusarik000 <40994451+dusarik000@users.noreply.github.com>
2024-10-15 14:12:08 +0000

    set resize messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import pandas as pd
 import math
 from pathlib import Path
 
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='GDP dashboard',
+    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+)
 st.markdown("""
     <script>
     function sendHeight() {
@@ -13,13 +18,6 @@ st.markdown("""
     window.addEventListener('resize', sendHeight);
     </script>
     """, unsafe_allow_html=True)
-
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
 
